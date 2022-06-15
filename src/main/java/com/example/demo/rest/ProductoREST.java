471c0008cd95206d24f284a46372aa9d6cff26fe
rogerespinoza1 <86305792+rogerespinoza1@users.noreply.github.com>
2022-06-14 22:52:49 -0500

    added github action 6

--- src/main/java/com/example/demo/rest/ProductoREST.java
+++ src/main/java/com/example/demo/rest/ProductoREST.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,8 +24,8 @@ public class ProductoREST {
 	private ProductoService productoService;
 	
 	@GetMapping
-	private ArrayList<Producto> ListarProductos(){
-		return productoService.findAllProducto();
+	private ResponseEntity<ArrayList<Producto>> ListarProductos(){
+		return ResponseEntity.ok(productoService.findAllProducto());
 	}	
 	@PostMapping
 	private Producto guardar(@RequestBody Producto producto ){
